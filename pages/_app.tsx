e217ad39960367dade9481f20561089986ddf346
ouo_yoonk <joyk9251@gmail.com>
2021-04-28 14:55:17 +0900

    styled-component ThemePrivider 적용[윤경] - _app.js에 ThemePrivider 적용 - theme.ts 파일 생성 및 스타일 적용 확인

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { AppProps } from 'next/app'
-
+import { ThemeProvider } from 'styled-components';
 import '../styles/reset.css';
+import styled from 'styled-components';
 
 // store
 import wrapper from '../redux/configureStore'
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
-      <h1>리액트</h1>
-      <Component {...pageProps} />
-    </React.Fragment>
+    <ThemeProvider theme={theme} >
+      <React.Fragment>
+        <h1>리액트</h1>
+        <Component {...pageProps} />      
+      </React.Fragment>
+    </ThemeProvider>
   )
 }
+
+
 
 export default wrapper.withRedux(App);
---
+++ styles/theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+const theme : DefaultTheme = {
+  color:{
+    main: '#5D6E91',
+    yellow: '#F9964f',
+    gray: '#EEEDED',
+    mainBlur:'#E7F0F9',
+    mainThick:'#121B46',
+  },
+  view : {
+    mobile: `(max-width: 767px)`,
+    tablet: `(max-width: 1024px)`,
+    desktop: `(min-width: 1025px)`
+  },
+  flex : {
+    column : 'display: flex; flex-direction:column; align-items: center; justify-content: space-between; ',
+    row : 'display: flex; align-items: center; justify-content: space-between;',    
+  },
+  border_box: `box-sizing:border-box;`
+};
+
+export default theme;
