db66b1227c17ac49b5d559c66be28c79e46c6e5c
Jaemin Jeon <wjswoals777@icloud.com>
2021-04-29 16:03:55 +0900

    weather api 세팅 [재민]
    1. shared/api 에 weatherAPI 선언
    2. _app.tsx 에서 호출 확인

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components';
 import '../styles/reset.css';
@@ -8,12 +8,23 @@ import styled from 'styled-components';
 import wrapper from '../redux/configureStore'
 import theme from 'styles/theme';
 
+import { weatherAPI } from '../shared/api';
+
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    getWeather()
+  }, [])
+
+  const getWeather = async () => {
+    console.log('getWeather')
+    const res = await weatherAPI.getWeather(37.6027, 126.9291)
+    console.log(res.data)
+  }
   return (
     <ThemeProvider theme={theme} >
       <React.Fragment>
         <h1>리액트</h1>
-        <Component {...pageProps} />      
+        <Component {...pageProps} />
       </React.Fragment>
     </ThemeProvider>
   )
