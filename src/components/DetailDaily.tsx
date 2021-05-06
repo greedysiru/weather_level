e0a6891da8af56d7cb9b27796ed48d5ebc6d4f63
ouo_yoonk <joyk9251@gmail.com>
2021-05-06 20:06:14 +0900

    DetailDaily[윤경] - swpie 적용중

--- src/components/DetailDaily.tsx
+++ src/components/DetailDaily.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { useSelector } from 'react-redux'
+import React from 'react';
+
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from 'swiper/core';
+// Swipter styles
+import 'swiper/swiper.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import { useSelector } from 'react-redux';
 import { RootState } from '../redux/modules';
 
-const DetailDaily = props => {
-    const {category} = props
-    // category : rainPer, tmp, weather
-    const state = useSelector((state: RootState) => state.weather.weatherInfo?.dayInfo)
-    console.log(state)
-    return (
-        <div>
-            {category}
-        </div>
-    )
-}
+const DetailDaily = (props) => {
+  const { category } = props;
+  // category : rainPer, tmp, weather
+  const state = useSelector((state: RootState) => state.weather.weatherInfo?.dayInfo);
+  console.log(state);
+  return <div>{category}</div>;
+};
 
-
-export default DetailDaily
+export default DetailDaily;
