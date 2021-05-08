40c3c8acefdf68a066aa27d2a1d23ccbc7d8071a
Jaemin Jeon <wjswoals777@icloud.com>
2021-05-08 16:16:31 +0900

    토큰 null 처리 [재민]
    로컬스토리지에서 값이 없을 때 null 이면 공백 문자열로 보내도록 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -14,16 +14,7 @@ const theme: DefaultTheme = {
     gray1: '#DCDCDC',
     gray2: '#C4C4C4',
     gray3: '#939393',
-<<<<<<< HEAD
-    sky1: '#FFD459',
-    sky2: '#73ABFF',
-    sky3: '#738FFF',
-    sky4: '#305580',
-    sky5: '#606680',
-    background: '#F6F7FB',
-=======
     purple: '#EBEBF9',
->>>>>>> 818d36420f9a61d3497b6d0ccb46ce9ef2efcad4
   },
   view: {
     mobile: `(max-width: 767px)`,
@@ -35,12 +26,8 @@ const theme: DefaultTheme = {
     column: 'display: flex; flex-direction:column; align-items: center; justify-content: space-between; ',
     row: 'display: flex; align-items: center; justify-content: space-between;',
   },
-<<<<<<< HEAD
-  border_box: `box-sizing:border-box;`,
-=======
   shadow: `box-shadow: 8px 8px 16px 4px rgba(133, 139, 146, 0.06)`,
   border_box: `box-sizing:border-box;`
->>>>>>> 818d36420f9a61d3497b6d0ccb46ce9ef2efcad4
 };
 
 export default theme;
