beb581bdf900882949fdffb7ccd5a9ec128643c7
Jaemin Jeon <wjswoals777@icloud.com>
2021-05-10 14:30:57 +0900

    LocationSetting.tsx 비구조화 [재민]

--- src/pages/LocationSetting.tsx
+++ src/pages/LocationSetting.tsx
@@ -21,7 +21,7 @@ const LocationSetting = (props) => {
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
   const [deleteList, setDeleteList] = useState([]);
 
-  const userLocationInfo = useSelector((state: RootState) => state.location.userLocationInfo);
+  const { userLocationInfo } = useSelector((state: RootState) => state.location);
 
   useEffect(() => {
     dispatch(locationActions.fetchUserRegion());
@@ -30,7 +30,6 @@ const LocationSetting = (props) => {
   useEffect(() => {
     if (userLocationInfo) {
       const current = userLocationInfo.currentRegion;
-
       const fullResionName = `${current.bigRegionName} ${current.smallRegionName}`;
       setCurrentRegion(fullResionName);
 
