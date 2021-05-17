f91c67b7faf2d2488da91aefbb1293ad4b4405b1
Jaemin Jeon <wjswoals777@icloud.com>
2021-05-17 21:51:59 +0900

    Fit.tsx 이미지 크기 contain 변경 [재민]

--- src/components/Fit.tsx
+++ src/components/Fit.tsx
@@ -23,6 +23,9 @@ const FitWrap = styled.div`
 width: 90%;
 height: 85%;
 margin: 0 auto 0 auto;
+display: flex;
+justify-content: center;
+align-items: center;
 background-color: white;
 border-radius: 15.5px;
 border: 1px solid ${(props) => props.theme.color.purple};
@@ -30,9 +33,9 @@ border: 1px solid ${(props) => props.theme.color.purple};
 
 const FitImage = styled.div`
 background-image: url(${weatherFit});
-width: 100%;
-height: 100%;
-background-size: auto;
+width: 90%;
+height: 90%;
+background-size: contain;
 background-repeat: no-repeat;
 background-position: center center;
 `
