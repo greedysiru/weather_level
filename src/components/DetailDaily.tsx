855e4311d22d55a71ae20b242b684fecdefa32dd
ouo_yoonk <joyk9251@gmail.com>
2021-05-18 12:17:14 +0900

    DetailDaily [윤경] - title argin 수정

--- src/components/DetailDaily.tsx
+++ src/components/DetailDaily.tsx
@@ -10,7 +10,7 @@ import 'swiper/swiper.min.css';
 import 'swiper/components/pagination/pagination.min.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/modules';
-import { Grid, LongCard } from './elements';
+import { Grid, LongCard, Title } from './elements';
 
 // common
 import { convertWeaterInfo } from '../shared/common';
@@ -124,17 +124,17 @@ const DetailDaily = (props) => {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'flex-start',
   };
   if (isDesktopMode) {
     return (
       <Grid height="90%">
         <Grid isColumn width="50%" jc="space-between" ai="center" height="100%">
-          <Title>시간별 {title[category]} </Title>
+          <Grid margin="0.5rem 0rem">
+            <Title>시간별 {title[category]} </Title>
+          </Grid>
           <Grid isColumn padding="0 1.5rem" height="100%" overFlow jc="center">
-            <TimeListWrap isDesktopMode>
-              {timeListComponent}
-            </TimeListWrap>
+            <TimeListWrap isDesktopMode>{timeListComponent}</TimeListWrap>
           </Grid>
         </Grid>
         <Grid isColumn padding="0 1.5rem" width="50%" jc="space-between" ai="center" height="100%">
@@ -150,19 +150,21 @@ const DetailDaily = (props) => {
         {/* 첫번째 슬라이드 */}
 
         <SwiperSlide style={slideStyle as React.CSSProperties}>
-          <Title>시간별 {title[category]} </Title>
-          <Grid padding='1.5rem 1.5rem 0 1.5rem' height="100%" overFlow ai="space-between">
-            <TimeListWrap>
-              {timeListComponent}
-            </TimeListWrap>
+          <Grid margin="1rem 0rem">
+            <Title>시간별 {title[category]} </Title>
+          </Grid>
+          <Grid padding="0rem 1.5rem 0 1.5rem" height="100%" overFlow ai="space-between">
+            <TimeListWrap>{timeListComponent}</TimeListWrap>
           </Grid>
         </SwiperSlide>
 
         {/* 두번째 슬라이드 */}
 
         <SwiperSlide style={slideStyle as React.CSSProperties}>
-          <Title> 일별 {title[category]} </Title>
-          <Grid padding='1.5rem 1.5rem 3rem 1.5rem' isColumn height="100%" jc="space-between">
+          <Grid margin="1rem 0rem">
+            <Title> 일별 {title[category]} </Title>
+          </Grid>
+          <Grid padding="0rem 1.5rem 3rem 1.5rem" isColumn height="100%" jc="space-between">
             {weeklyListComponent}
           </Grid>
         </SwiperSlide>
@@ -178,21 +180,18 @@ const Container = styled.div`
   justify-content: space-around;
 `;
 
-const Title = styled.div`
+/* const Title = styled.div`
   font-size: 1.8rem;
   font-weight: 600;
   margin: 1rem;
 `;
-
+ */
 const TimeListWrap = styled.div`
   padding: 0 1rem;
   display: flex;
   flex-direction: column;
   width: 100%;
-  ${(props) => props.isDesktopMode ?
-    (`height: 97%`) :
-    (`height: 92%`)
-  };  
+  ${(props) => (props.isDesktopMode ? `height: 97%` : `height: 92%`)};
   align-items: center;
   justify-content: space-between;
   background-color: white;
@@ -202,8 +201,6 @@ const TimeListWrap = styled.div`
     display: none;
   }
   ${(props) => props.theme.shadow};
-`
-
-
+`;
 
 export default DetailDaily;
