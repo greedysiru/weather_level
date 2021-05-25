624cbb2ffbca7693634435697d3121b400e13e58
Jaemin Jeon <wjswoals777@icloud.com>
2021-05-25 15:37:42 +0900

    아이콘 이미지 크기 수정 [재민]

---
+++ src/redux/modules/slider.ts
@@ -0,0 +1,23 @@
+import { createReducer, createAction, PayloadAction } from '@reduxjs/toolkit';
+
+// 슬라이더의 위치를 설정하는 모듈
+
+// slider 타입 선언
+
+type sliderType = {
+
+};
+
+export const initialState: sliderType = {
+
+};
+
+const slider = createReducer(initialState, {
+
+});
+
+export const sliderActions = {
+
+};
+
+// export default slider;
