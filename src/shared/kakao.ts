32fb9554efd61bef07b0e876011ce73e47729435
Jaemin Jeon <greedysiru@gmail.com>
2022-04-03 18:09:56 +0900

    [재민] AWS Amplify Setting
    1. gitignore
    - kakao key를 저장하던 파일인 config.ts 삭제
    - kakao key를 .env.development에 저장
    
    2. kakao.ts
    - Kakao 초기화 시 키값을 환경 변수에서 가져오기

--- src/shared/kakao.ts
+++ src/shared/kakao.ts
@@ -1,8 +1,6 @@
-import KAKAO_KEY from './config';
-
 // 카카오 링크 객체 불러오기
 const { Kakao } = window;
 
 export default function initialize() {
-  Kakao.init(KAKAO_KEY);
+  Kakao.init(process.env.REACT_APP_KAKAO_KEY);
 }
